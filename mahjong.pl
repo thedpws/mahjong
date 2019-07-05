35805341c7e998835036d9ae5a9fb45d079c960d
AZ Vasquez <azvasquez99@gmail.com>
2019-07-05 08:33:00 -0400

    Sorting.

--- mahjong.pl
+++ mahjong.pl
@@ -1,3 +1,16 @@
+isEquivalent(Hand1, Hand2, Equivalency) :- quicksort(Hand1, Hand1Sorted), quicksort(Hand2, Hand2Sorted), isEquivalentHelp(Hand1Sorted, Hand2Sorted, Equivalency).
+isEquivalentHelp([H|T1], [H|T2], Equivalency) :- isEquivalentHelp(T1, T2, Equivalency).
+isEquivalentHelp([], [], true).
+
+
+% Quicksort
+pivot(_, [], [], []).
+pivot(Pivot, [Head|Tail], [Head|LessThan], GreaterThanOrEqualTo) :- not(isLessThan(Pivot, Head)), pivot(Pivot, Tail, LessThan, GreaterThanOrEqualTo). 
+pivot(Pivot, [Head|Tail], LessThan, [Head|GreaterThanOrEqualTo]) :- isLessThan(Pivot, Head), pivot(Pivot, Tail, LessThan, GreaterThanOrEqualTo).
+
+quicksort([], []).
+quicksort([Head|Tail], Sorted) :- pivot(Head, Tail, List1, List2), quicksort(List1, SortedList1), quicksort(List2, SortedList2), append(SortedList1, [Head|SortedList2], Sorted).
+
 quantity(X, 4) :- isTile(X).
 
 isRun(X) :- X=[Tile1,Tile2,Tile3], value(Tile1, V0), value(Tile2, V0+1), value(Tile3, V0+2).
@@ -94,3 +107,25 @@ wind(south_wind, south).
 wind(west_wind, west).
 wind(north_wind, north).
 
+%  ____                                 _                     
+% / ___|___  _ __ ___  _ __   __ _ _ __(_)___  ___  _ __  ___ 
+%| |   / _ \| '_ ` _ \| '_ \ / _` | '__| / __|/ _ \| '_ \/ __|
+%| |__| (_) | | | | | | |_) | (_| | |  | \__ \ (_) | | | \__ \
+% \____\___/|_| |_| |_| .__/ \__,_|_|  |_|___/\___/|_| |_|___/
+%                     |_|                                     
+
+isLessThan(L, G) :- isSuit(L), isHonor(G).                  % Suits < Honors
+isLessThan(L, G) :- suit(L, bamboo), suit(G, circle).       % Bamboo < Circle < Character
+isLessThan(L, G) :- suit(L, bamboo), suit(G, character). 
+isLessThan(L, G) :- suit(L, circle), suit(G, character).
+isLessThan(L, G) :- suit(L, Suit), suit(G, Suit), value(L, V0), value(G, V1), V1 > V0.    % 1 < 2 < ... < 9
+isLessThan(L, G) :- dragon(L, _), wind(G, _).               % Dragon < Wind
+isLessThan(L, G) :- dragon(L, white), dragon(G, red).         % White < Red < Green
+isLessThan(L, G) :- dragon(L, white), dragon(G, green).         % White < Red < Green
+isLessThan(L, G) :- dragon(L, red), dragon(G, green).
+isLessThan(L, G) :- wind(L, east), wind(G, south).              % East < South < West < North
+isLessThan(L, G) :- wind(L, east), wind(G, west).
+isLessThan(L, G) :- wind(L, east), wind(G, north).
+isLessThan(L, G) :- wind(L, south), wind(G, west).
+isLessThan(L, G) :- wind(L, south), wind(G, north).
+isLessThan(L, G) :- wind(L, west), wind(G, north).
